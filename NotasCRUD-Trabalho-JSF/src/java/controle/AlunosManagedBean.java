0522f7635dd0b66e350f7c612b27b9edd1b4bfcb
Caio Silva <caiovosilva@outlook.com>
2018-09-03 19:42:50 -0300

    implementacao do grafico em bolha

--- NotasCRUD-Trabalho-JSF/src/java/controle/AlunosManagedBean.java
+++ NotasCRUD-Trabalho-JSF/src/java/controle/AlunosManagedBean.java
@@ -23,6 +23,8 @@ import org.primefaces.event.RowEditEvent;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.BubbleChartModel;
+import org.primefaces.model.chart.BubbleChartSeries;
 import org.primefaces.model.chart.ChartSeries;
 
 /**
@@ -43,6 +45,7 @@ public class AlunosManagedBean implements Serializable{
     public void init() {
         atualizar();
         createBarModel();
+        createBubbleModel();
     }
     
     public List<Aluno> getAlunos(){
@@ -78,6 +81,7 @@ public class AlunosManagedBean implements Serializable{
     public void atualizar(){
         listaAlunos = gerenciadorAluno.getAllAlunos();
         createBarModel();
+        createBubbleModel();
     }
     
     public void cadastrar(){
@@ -114,6 +118,10 @@ public class AlunosManagedBean implements Serializable{
         return gerenciadorAluno;
     }
     
+    public BubbleChartModel getBubbleModel() {
+        return bubbleModel;
+    }
+    
     private void createBarModel() {
         barModel = new BarChartModel();
         ChartSeries notas = new ChartSeries();
@@ -121,24 +129,38 @@ public class AlunosManagedBean implements Serializable{
         notas.set("Nota 1", gerenciadorAluno.mediaNota1());
         notas.set("Nota 2", gerenciadorAluno.mediaNota2());
         notas.set("Nota 3", gerenciadorAluno.mediaNota3());
-      
-
-        barModel.addSeries(notas);
-         
-         
+        barModel.addSeries(notas);      
         barModel.setTitle("MÉDIA DE NOTAS DOS ALUNOS");
-        barModel.setLegendPosition("ne");
-         
-        Axis xAxis = barModel.getAxis(AxisType.X);
-        //xAxis.setLabel("Gender");
-         
+        barModel.setLegendPosition("ne");   
         Axis yAxis = barModel.getAxis(AxisType.Y);
         yAxis.setLabel("Média");
         yAxis.setMin(0);
         yAxis.setMax(10);
     }
     
+    private void createBubbleModel(){
+        bubbleModel = new BubbleChartModel();
+        for(Aluno aluno: listaAlunos){
+            double media = ((aluno.getNota1() + aluno.getNota2() + aluno.getNota3()) / 3);
+            int intMedia = (int) Math.round(media*10);
+            bubbleModel.add(new BubbleChartSeries(aluno.getNome(), listaAlunos.indexOf(aluno)+1, intMedia, intMedia));
+        }    
+        
+        bubbleModel.setTitle("Bubble Chart");
+        Axis yAxis = bubbleModel.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(100);
+        yAxis.setLabel("Média");
+        Axis xAxis = bubbleModel.getAxis(AxisType.X);
+        xAxis.setMin(0);
+        xAxis.setMax(listaAlunos.size());
+        xAxis.setLabel("Lugar na lista");
+     
+    }
+     
+    
     private List<Aluno> listaAlunos;
     private GerenciadorAluno gerenciadorAluno;
     private BarChartModel barModel;
+    private BubbleChartModel bubbleModel;
 }
