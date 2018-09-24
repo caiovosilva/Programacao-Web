4fe02001448050989ad82e09da48d60c6617ac1e
Caio Silva <caiovosilva@outlook.com>
2018-09-24 19:31:30 -0300

    complementando projetos

--- Python/projetos/agendaTelefonicaCRUD/app/urls.py
+++ Python/projetos/agendaTelefonicaCRUD/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.list, name='list.html'),
+    # path('app/list', views.list, name='list'),
 ]
--- Python/projetos/agendaTelefonicaCRUD/app/views.py
+++ Python/projetos/agendaTelefonicaCRUD/app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from .models import Pessoa
 def home(request):
     return render(request, 'app/home.html', {})
+
+def list(request):
+    pessoas = Pessoa.objects.order_by('nome')
+    return render(request, 'app/list.html', {'pessoas':pessoas})
+    # return render(request, 'app/list.html', {})
